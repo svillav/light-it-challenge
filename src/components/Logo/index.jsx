d05b93758b52554774547ed91d4e0d7c0139e816
Santiago Villa <santiagovillavega@gmail.com>
2022-08-08 19:02:15 -0300

    Logo component creation

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../assets/logo.png"
+
+const Logo = () => {
+  return (
+    <div>
+      <img className="h-16 w-50" src={logo} />
+    </div>
+  )
+}
+
+export default Logo;
