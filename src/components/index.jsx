2caecd035bf9048fa8381a340f8cc4613eda3b91
Santiago Villa <santiagovillavega@gmail.com>
2022-08-08 19:08:11 -0300

    Components index

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Position from "./Position";
+import Zoom from "./Zoom";
+import Main from "./Main";
+
+export { Navbar, Sidebar, Position, Zoom, Main };
